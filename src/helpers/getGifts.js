6cf80ed6a0c5e5d75d654bca3ccec9274e05832c
Sebastian Hidalgo <sebastianh.ing@gmail.com>
2021-06-02 18:11:54 -0500

    First commit

---
+++ src/helpers/getGifts.js
@@ -0,0 +1,18 @@
+//Funcion Conexion API
+export const getGifs = async ( category )=>{
+        
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=qatp2whm3ek18oFrEcYa3Aa9dGphDgZt`;
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img=>{
+        return { 
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return  gifs;
+}
